092960b69c5ca29bcece7a1937539d1319b06522
James Yarnell <james.yarnell1@bayer.com>
2024-09-21 06:45:35 -0500

    Auto commit changes

--- scripts/homebrew/dump.sh
+++ scripts/homebrew/dump.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 brew update
 
@@ -6,3 +6,15 @@ brew bundle dump --file=~/scripts/homebrew/Brewfile --force
 
 #restore
 # brew bundle --file=~/scripts/homebrew/Brewfile
+
+# Check for git changes
+if [[ $(git status --porcelain) ]]; then
+  # Add all changes
+  git add .
+
+  # Commit changes
+  git commit -m "Auto commit changes"
+
+  # Push changes
+  git push
+fi
